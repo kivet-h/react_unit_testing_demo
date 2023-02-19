29640c0ab2e1d50304aeec70878c03e38fbdfc3c
kivet <kivet666@dingtalk.com>
2023-02-19 17:55:40 +0800

    feat: jest配置，设置覆盖率

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,11 @@ module.exports = {
   collectCoverage: true,
   // 告诉 jest 哪些文件需要经过单元测试测试
   collectCoverageFrom: ['src/utils/**/*'],
+  // coverageThreshold: {
+  //   global: {
+  //     statements: 90, // 保证每个语句都执行了
+  //     functions: 90, // 保证每个函数都调用了
+  //     branches: 90, // 保证每个 if 等分支代码都执行了
+  //   },
+  // },
 };
